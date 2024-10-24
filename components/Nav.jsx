58c534e14b155145d3ad8c3f6d5ac52698770cad
Rukky <abrukevwe@gmail.com>
2024-10-24 09:48:39 +0100

    Auth Working

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession()
 
-    const [providers, setProvider] = useState(null);
+    const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
 
             setProviders(response);
         }
-        setProviders();
+        setUpProviders();
     }, [])
 
     return (
@@ -31,10 +31,11 @@ const Nav = () => {
                 <p className="logo_text">Promptopia</p>
             </Link>
 
+          
             {/* Desktop Navigation */}
 
             <div className="sm:flex hidden">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
                         <Link href='/create-prompt' className="black_btn">Create Post</Link>
                         <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
@@ -59,7 +60,7 @@ const Nav = () => {
             </div>
             {/* Mobile Navigation */}
             <div className="sm:hidden flex relative">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex">
                         <Image
                             src="/assets/images/logo.svg"
