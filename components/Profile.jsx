bf1cf29be99902faed1e999749d07556a8188942
Rukky <abrukevwe@gmail.com>
2024-11-10 03:48:07 +0100

    ui

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
     <section className='w-full'>
       <h1 className='head_text text-left'>
-        <span className='blue_gradient sm:whitespace-pre-wrap sm:text-wrap'>{name} Profile</span>
+        <span className='sm:whitespace-pre-wrap sm:text-sm'>{name} Profile</span>
       </h1>
       <p className='desc text-left'>{desc}</p>
 
